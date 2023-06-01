927bcac737eb16b8994c52e6947906da9f6a6ddb
kyle-mucerino <kyle.mucerino@gmail.com>
2023-05-31 17:59:49 -0700

    models filled in Java

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 2.45;
+        String c = "string";
+        double sum = a + b;
+        String concat = a + c;
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
