25b2162cbf638414d815a004d01816def6c9efc0
kyle-mucerino <kyle.mucerino@gmail.com>
2023-06-01 12:56:48 -0700

    Post model fixed

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,9 +1,9 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.sun.istack.NotNull;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.persistence.*;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -13,7 +13,6 @@ import java.util.Objects;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "post")
 public class Post implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
@@ -24,21 +23,16 @@ public class Post implements Serializable {
     @Transient
     private int voteCount;
     private Integer userId;
-
     @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "posted_at")
     private Date postedAt = new Date();
-
     @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "updated_at")
     private Date updatedAt = new Date();
-
-    // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
-
 
     public Post() {
     }
@@ -54,63 +48,48 @@ public class Post implements Serializable {
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getPostUrl() {
         return postUrl;
     }
-
     public void setPostUrl(String postUrl) {
         this.postUrl = postUrl;
     }
-
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
-
     public int getVoteCount() {
         return voteCount;
     }
-
     public void setVoteCount(int voteCount) {
         this.voteCount = voteCount;
     }
-
     public Integer getUserId() {
         return userId;
     }
-
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
-
     public Date getPostedAt() {
         return postedAt;
     }
-
     public void setPostedAt(Date postedAt) {
         this.postedAt = postedAt;
     }
-
     public Date getUpdatedAt() {
         return updatedAt;
     }
-
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
@@ -118,7 +97,6 @@ public class Post implements Serializable {
     public List<Comment> getComments() {
         return comments;
     }
-
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
@@ -126,7 +104,7 @@ public class Post implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Post)) return false;
+        if(!(o instanceof Post)) return false;
         Post post = (Post) o;
         return getVoteCount() == post.getVoteCount() &&
                 Objects.equals(getId(), post.getId()) &&
@@ -148,6 +126,7 @@ public class Post implements Serializable {
     public String toString() {
         return "Post{" +
                 "id=" + id +
+                ", postUrl='" + postUrl + '\'' +
                 ", title='" + title + '\'' +
                 ", postUrl='" + postUrl + '\'' +
                 ", userName='" + userName + '\'' +
--- src/main/java/com/technews/repository/PostRepository.java
+++ src/main/java/com/technews/repository/PostRepository.java
@@ -1,6 +1,5 @@
 package com.technews.repository;
 
-import com.technews.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
