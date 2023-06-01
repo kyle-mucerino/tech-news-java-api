81f14fa63f49ff4a9d40c63ffb2f7727b9e7ba55
kyle-mucerino <kyle.mucerino@gmail.com>
2023-06-01 11:55:58 -0700

    User model fixed

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,17 +1,15 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
+import java.util.List;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
 public class User implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
@@ -21,15 +19,10 @@ public class User implements Serializable {
     private String password;
     @Transient
     boolean loggedIn;
-
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Post> posts;
-
-    // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Vote> votes;
-
-    // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
 
@@ -43,74 +36,58 @@ public class User implements Serializable {
         this.password = password;
     }
 
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getUsername() {
         return username;
     }
-
-    public void setUsername(String username) {
+    public void setUsername() {
         this.username = username;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public boolean isLoggedIn() {
         return loggedIn;
     }
-
     public void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
-
     public List<Post> getPosts() {
         return posts;
     }
-
     public void setPosts(List<Post> posts) {
         this.posts = posts;
     }
-
     public List<Vote> getVotes() {
         return votes;
     }
-
     public void setVotes(List<Vote> votes) {
         this.votes = votes;
     }
-
     public List<Comment> getComments() {
         return comments;
     }
-
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if(this == o) return true;
         if (!(o instanceof User)) return false;
         User user = (User) o;
         return isLoggedIn() == user.isLoggedIn() &&
@@ -132,7 +109,7 @@ public class User implements Serializable {
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
+                ", username'" + username + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", loggedIn=" + loggedIn +
--- src/main/java/com/technews/repository/UserRepository.java
+++ src/main/java/com/technews/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.technews.repository;
 
-import com.technews.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
